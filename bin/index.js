b2b438e6b1c917adfeb623a5632acc2cec519d0f
nathanfaucett <nathanfaucett@gmail.com>
2015-10-29 11:11:48 -0500

    updates

--- bin/index.js
+++ bin/index.js
@@ -20,18 +20,16 @@ if (!options.out) {
     throw new Error("out file require");
 }
 
-comn(options.file, {
-    exportName: options.exportName,
-    parseAsync: false,
-    ignore: options.ignore
-}, function onComn(error, out) {
-    if (error) {
-        throw error;
-    } else {
-        fileUtils.writeFile(options.out, out, function onWriteFile(error) {
-            if (error) {
-                throw error;
-            }
-        });
+fileUtils.writeFile(
+    options.out,
+    comn(options.file, {
+        exportName: options.exportName,
+        parseAsync: false,
+        ignore: options.ignore
+    }),
+    function onWriteFile(error) {
+        if (error) {
+            throw error;
+        }
     }
-})
+);
