3a2fd365b2e6fe812d343d9fb55ac52bf98a7ffd
nathanfaucett <nathanfaucett@gmail.com>
2015-10-31 14:40:21 -0500

    updates

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 var comn = require(".."),
+    isString = require("is_string"),
     fileUtils = require("file_utils"),
     argv = require("argv");
 
 
 var options = argv({
     file: ["f", "start file", "string"],
-    out: ["o", "out file", "string"],
+    out: ["o", "out directory/file", "string"],
     ignore: ["i", "ignore paths", "array"],
     exportName: ["e", "export to global scope", "string"]
 }).parse();
@@ -15,21 +16,19 @@ var options = argv({
 if (!options.file) {
     throw new Error("input file require");
 }
-
 if (!options.out) {
     throw new Error("out file require");
 }
 
-fileUtils.writeFile(
-    options.out,
-    comn(options.file, {
-        exportName: options.exportName,
-        parseAsync: false,
-        ignore: options.ignore
-    }),
-    function onWriteFile(error) {
-        if (error) {
-            throw error;
-        }
+var out = comn(options.file, {
+    exportName: options.exportName,
+    ignore: options.ignore
+});
+
+if (isString(out)) {
+    fileUtils.writeFileSync(options.out, out);
+} else {
+    for (var path in out) {
+        fileUtils.writeFileSync(options.out + "/" + path, out[path]);
     }
-);
+}
